cbe82221360f90b44a3970903d9ad3314b7fd355
Ilya Rudyak <ilyarudyak@yahoo.com>
2017-02-20 11:48:24 +0300

    clean dir

--- oreilly_web_scraping_book/ch1/scrapetest2.py
+++
@@ -1,7 +0,0 @@
-from bs4 import BeautifulSoup
-from urllib.request import urlopen
-
-
-html = urlopen("http://pythonscraping.com/pages/page1.html")
-bsObj = BeautifulSoup(html.read(), "html.parser");
-print(bsObj.h1)
